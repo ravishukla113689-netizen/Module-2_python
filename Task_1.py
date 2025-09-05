4369bde212f9582d447aa529a4bfe3001debc9d3
ravishukla113689-netizen <ravi.shukla113689@gmail.com>
2025-09-05 22:38:44 +0530

    Create Task_1.py

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first Number : "))
+b = int(input("Enter the Second Number : "))
+
+add = a + b
+sub = a - b
+multi = a * b
+div = a / b
+
+print(f"The Addition : {add}")
+print(f"The Addition : {sub}")
+print(f"The Addition : {multi}")
+print(f"The Addition : {div}")
